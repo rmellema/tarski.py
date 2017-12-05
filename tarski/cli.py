a89b70b52fec6549074055645d63fb1ec5e72282
rmellema <rene.mellema@gmail.com>
2017-12-05 15:07:08 +0100

    Changed the program so it is able to read in different input formats

--- tarski/cli.py
+++ tarski/cli.py
@@ -2,18 +2,68 @@
 A module that holds all the material needed for running the script.
 """
 from argparse import ArgumentParser
+import pkg_resources
+import sys
 
 from tarski.model import Model
 from tarski.read.tarski import TarskiWorldReader
 
-def read_file(file):
+def get_plugins(group):
+    """
+    Get all the registered plugins.
+    """
+    return [entry.load() for entry in pkg_resources.iter_entry_points(group)]
+
+def get_input_plugins():
+    """
+    Get all the plugins for loading files.
+    """
+    return get_plugins("tarski.readers")
+
+def list_input_formats():
+    """
+    List all the input formats that the program understands.
+    """
+    format_string = "{:<20}{}"
+    print(format_string.format("Format", "Description"))
+    print('-' * 80)
+    for plugin in get_input_plugins():
+        print(format_string.format(plugin.format_name, plugin.format_description))
+
+def find_plugin(format_):
+    """
+    Find the plugin with name `format_`.
+    """
+    for plugin in get_input_plugins():
+        if plugin.format_name == format_:
+            return plugin
+
+def read_file(format_, file_):
     """
     Read in the world as specified in `file`.
     """
-    with open(file) as stream:
-        reader = TarskiWorldReader(stream=stream)
+    with open(file_) as stream:
+        reader = format_(stream=stream)
         world = reader.read_world()
         return Model(world=world)
+
+def convert(files, iformat, output, use_header=None):
+    """
+    Convert all the files in `files` into the corresponding first order structures and write them to
+    the output.
+
+    :param list[string] files: A list of file names that need to be converted.
+    :param output: The stream to write the output to.
+    :param bool use_header: Whether or not to write a header for each file.
+    """
+    if len(files) > 1 and use_header is None:
+        use_header = True
+    for file_ in files:
+        model = read_file(iformat, file_)
+        if use_header:
+            output.write('# {}\n'.format(file_))
+        output.write(str(model))
+        output.write('\n')
 
 def main():
     """
@@ -22,14 +72,22 @@ def main():
     parser = ArgumentParser(description="""
     A utility for translating Tarski's World files into their corresponding first order structures.
     """)
-    parser.add_argument("files", help="The Tarski's World file to convert.", nargs="+")
+    parser.add_argument("files", help="The Tarski's World file to convert.", nargs='*')
+    parser.add_argument("--list-input-formats", action="store_true",
+                        help="Give a list of the accepted input formats and exit")
+    parser.add_argument("-f", "--format", default='tarski',
+                        help="The format of the input file, defaults to tarski")
     args = parser.parse_args()
-    use_header = True
-    for file in args.files:
-        model = read_file(file)
-        if use_header:
-            print('# {}'.format(file))
-        print(str(model))
+    if args.list_input_formats:
+        list_input_formats()
+        return 0
+    if not args.files:
+        parser.error("the following arguments are required: files")
+    plugin = find_plugin(args.format)
+    if plugin is None:
+        parser.error("unrecognized format: {}".format(args.format))
+    use_header = None
+    convert(args.files, TarskiWorldReader, sys.stdout, use_header)
 
 if __name__ == '__main__':
     main()
